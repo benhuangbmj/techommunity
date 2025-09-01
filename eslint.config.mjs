8790d1eb4a26aac30dc297f02714d0c527198fe8
benhuangbmj <benjaminwong1985@gmail.com>
2025-09-01 08:56:21 -0400

    chore: code style reinforcement

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +14,38 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ),
+
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      "simple-import-sort": simpleImportSort,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-duplicate-imports": "warn",
+      "no-console": "warn",
+      "simple-import-sort/imports": "warn",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
   {
     ignores: [
       "node_modules/**",
